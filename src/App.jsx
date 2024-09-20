063c561d55a4b8edb599de5fb7781f7598347a94
rifu28 <rifatmeumeu28@gmail.com>
2024-09-20 23:51:14 +0600

    after two days

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   const coffees = useLoaderData();
   return (
     <>
-      <h1>coffee serial:{coffees.length}</h1>
-      <div className="grid grid-cols-3">
+      <h1 className="text-center my-5 font-semibold text-3xl text-yellow-900">
+        Coffee Collection
+      </h1>
+      <div className="grid md:grid-cols-3 w-full gap-14 mx-auto">
         {coffees.map((coffee) => (
           <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
         ))}
