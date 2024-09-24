16fb933cc137278756af0c0f99a322bad75ce54f
rifu28 <rifatmeumeu28@gmail.com>
2024-09-24 20:21:34 +0600

    loader data

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,7 +1,10 @@
+import { useLoaderData } from "react-router-dom";
+
 const Users = () => {
+  const users = useLoaderData();
   return (
     <div>
-      <h2>users</h2>
+      <h2>users:{users.length}</h2>
     </div>
   );
 };
